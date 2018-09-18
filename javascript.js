69099577eebe70e80ef7616a9424a0a4d983c281
webdevsergo <40713461+webdevsergo@users.noreply.github.com>
2018-09-18 19:42:06 +0400

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,33 @@
+$(document).ready(function(){
+	$(document).bind( 'mousewheel', function (e) { 
+    var nt = $(document.body).scrollTop()-(e.deltaY*e.deltaFactor*100); 
+    e.preventDefault(); 
+    e.stopPropagation(); 
+    $(document.body).stop().animate( { 
+         scrollTop : nt 
+     } , 500 , 'easeInOutCubic' );  
+	});
+   $('.menu-btn').on('click', function(e) {
+  	e.preventDefault();
+  	$('.menu').toggleClass('menu_active');
+	})
+	$('.menu-btn').on('click', function(e){
+   	e.preventDefault();
+  	$('.menu-btn').toggleClass('menu-btn-active');
+	});
+	$('.feedback-slider').slick({
+			prevArrow:'<button type="button" class="slick-prev"> <img src="icon/left-arrow (1).png"> </button>',nextArrow:'<button type="button" class="slick-next"><img src="icon/right-arrow (1).png"></button>'
+	});
+	$('.element1').click(function(){
+       $('html, body').animate({scrollTop:$('.rat').position().top}, 2000);
+	});
+	$('.element2').click(function(){
+       $('html, body').animate({scrollTop:$('.me').position().top}, 2000);
+	});
+	$('.element3').click(function(){
+       $('html, body').animate({scrollTop:$('.catalog').position().top}, 2000);
+	});
+	$('.element4').click(function(){
+       $('html, body').animate({scrollTop:$('.feedback').position().top}, 2000);
+	});
+});
